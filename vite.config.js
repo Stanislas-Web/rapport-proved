f7bd30c481a18c1f88e9a22182d4b3c801b531e7
Stanislas-Web <57170716+Stanislas-Web@users.noreply.github.com>
2025-08-26 00:09:01 +0100

    update the code

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',   // ← écoute sur toutes les interfaces
+    port: 5173,        // ← port de ton choix
+    strictPort: true,  // ← échoue si 5173 déjà utilisé
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 5173,
+  }
 })
