7af9499cf819285faa84ef85236bfbae345cdd13
Stanislas-Web <57170716+Stanislas-Web@users.noreply.github.com>
2025-08-26 12:08:12 +0100

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
